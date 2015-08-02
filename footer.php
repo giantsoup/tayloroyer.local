6b967ab07b2c866ba8b6730798c2cf77f76d81d7
Taylor Oyer <tayloroyer@Taylors-MacBook-Pro.local>
2015-08-01 21:35:57 -0700

    deleted comments and other things to get a fresh start, also started using flexbox for the menu

--- footer.php
+++ footer.php
@@ -12,9 +12,6 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'tayloroyer' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'tayloroyer' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'tayloroyer' ), 'tayloroyer', '<a href="http://automattic.com/" rel="designer">Automattic</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
